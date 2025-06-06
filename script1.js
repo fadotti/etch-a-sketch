b2feba29bda772cb89378b60bfd7ee78b4a7bd6f
Federico Dotti <fede.dotti.94@gmail.com>
2025-06-06 15:16:59 +0000

    Center the grid and fix its size
    The grid's size will now remain constant
    regardless of the number of squares it contains.
    This will allow the user to select the size of the
    grid.

--- script1.js
+++ script1.js
@@ -1,6 +1,9 @@
 let N = 16;
 let gridSize = N*N;
 
+let borderConstant = 51 / (N + 1) //51 is a magic number, increase it to increase border size
+let paddingConstant = 512 / (N * 2) //512 is a magic number, increase to increase the size of the squares
+
 gridContainer = document.querySelector('#grid-container');
 
 for(let i = 1; i <= N; i++) {
@@ -10,6 +13,14 @@ for(let i = 1; i <= N; i++) {
     for(let j = 1; j <=N; j++) {
         let gridSquare = document.createElement('div');
         gridSquare.className = 'grid-square';
+        gridSquare.style.borderWidth = `${borderConstant}px`;
+        gridSquare.style.padding = `${paddingConstant}px`;
+        if(j != N) {
+            gridSquare.style.borderRight = '0px';
+        }
+        if(i != N) {
+            gridSquare.style.borderBottom = '0px';
+        }
         rowContainer.appendChild(gridSquare);
     }
 }
